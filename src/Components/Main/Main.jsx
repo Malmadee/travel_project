0dd29b3108eea249485493b34e3b7eeec316cf1d
harshana1117 <harshanalakshan1117@gmail.com>
2025-10-15 22:25:46 +0530

    Update Main.jsx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,8 @@
 import React, {useEffect} from 'react'
 import './main.css'
 
-
+import {HiOutlineLocationMarker} from 'react-icons/hi'
+import { HiOutlineClipboardCheck } from 'react-icons/hi'
 
 import img from '../Assets/img.jpg'
 import img2 from '../Assets/img2.jpg'
